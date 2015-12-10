e52da82689f9b9a0134db615ec23a60cfdfd1703
slightair <arksutite@gmail.com>
2015-12-10 23:21:43 +0900

    Create new project

---
+++ Buri/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Buri
+//
+//  Created by slightair on 2015/12/10.
+//  Copyright © 2015年 slightair. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Buri/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Buri
+//
+//  Created by slightair on 2015/12/10.
+//  Copyright © 2015年 slightair. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
